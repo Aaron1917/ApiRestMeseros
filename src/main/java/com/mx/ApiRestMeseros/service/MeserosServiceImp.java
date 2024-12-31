0d95ee323e772dc9137dfc91cfb85ba7629b6c1a
Aaron1917 <aaron1917@users.noreply.github.com>
2024-12-30 22:06:58 -0600

    Crecion de servico Meseros y su logica

---
+++ src/main/java/com/mx/ApiRestMeseros/service/MeserosServiceImp.java
@@ -0,0 +1,73 @@
+package com.mx.ApiRestMeseros.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.mx.ApiRestMeseros.model.Mesero;
+import com.mx.ApiRestMeseros.repository.MeserosRepository;
+
+@Service
+public class MeserosServiceImp {
+
+	@Autowired
+	MeserosRepository meserosRepo;
+	
+	@Transactional(readOnly = true)
+	public List<Mesero> listar(){
+		return meserosRepo.findAll();
+	}
+	
+	@Transactional(readOnly = true)
+	public String guardar(Mesero mesero){
+		String msg = "";
+		for (Mesero m:meserosRepo.findAll()) {
+			if (m.getIdMesero().equals(mesero.getIdMesero())) {
+				msg = "Id de mesero repetido";
+				break;
+			}
+			if (m.getNombre().equalsIgnoreCase(mesero.getNombre()) &&
+				m.getApm().equalsIgnoreCase(mesero.getApp()) && 
+				m.getApm().equalsIgnoreCase(mesero.getApm())) {
+				msg = "Nombre del mesero repetido";
+				break;
+			}
+		}
+		if (msg.isEmpty())
+			meserosRepo.save(mesero);
+		return msg;
+	}
+	
+	@Transactional(readOnly = true)
+	public Mesero buscar(long id){
+		return meserosRepo.findById(id).orElse(null);
+	}
+	
+	@Transactional
+	public String editar(Mesero mesero) {
+		String msg = "";
+		for (Mesero m:meserosRepo.findAll()) {
+			if (!m.getIdMesero().equals(mesero.getIdMesero())) {
+				msg = "No existe un mesero con esa ID";
+			}
+		}
+		if (msg.isEmpty())
+			meserosRepo.save(mesero);
+		return msg;
+	}
+	
+	@Transactional
+	public String elimiar(Mesero mesero) {
+		String msg = "";
+		for (Mesero m:meserosRepo.findAll()) {
+			if (!m.getIdMesero().equals(mesero.getIdMesero())) {
+				msg = "No existe un mesero con esa ID";
+			}
+		}
+		if (msg.isEmpty())
+			meserosRepo.delete(mesero);
+		return msg;
+	}
+}
